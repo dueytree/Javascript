bceb1cd3df44467d548f13ba9934bac40d6a8d44
dueytree <dueytree.00@gmail.com>
2021-01-18 22:36:32 +0900

    browser and javascript

---
+++ id_class_Tag/index.js
@@ -0,0 +1,28 @@
+// // id로 태그 선택하기
+// const myTag = document.getElementById('btns');
+// console.log(myTag);
+//
+// // class로 태그 선택하기
+// const myTags = document.getElementsByClassName('color-btn');
+// console.log(myTags);
+// console.log(myTags[1]);
+// console.log(myTags.length);
+//
+// for (let tag of myTags) {
+//     console.log(tag);
+// }
+//
+// // css 선택자로 태그 선택하기
+// const myTag = document.querySelector('.color-btn');
+// console.log((myTag));
+// const myTags = document.querySelectorAll('.color-btn');
+// console.log((myTags));
+// const myTags2 = document.getElementsByClassName('color-btn');
+// console.log((myTags2));
+//
+// 이벤트(Event)와 버튼 클릭
+// const btn = document.querySelector('#myBtn');
+// // 이벤트 핸들링(Event Handling)
+// btn.onclick = function () { // 이벤트 핸들러(Event Handler)
+//     console.log('Hello Codeit!');
+// }
